91ce666e764b8265e22a866eb3984ea16814cde0
NikolayKuts <survivenik@gmail.com>
2026-04-19 21:04:29 +0300

    Bump Android SDK version to 0.2.0

--- Android/Library/build.gradle.kts
+++ Android/Library/build.gradle.kts
@@ -36,7 +36,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.tapp.sdk"
             artifactId = "tapp-sdk-android"
-            version = "0.1.0"
+            version = "0.2.0"
 
             afterEvaluate {
                 from(components["release"])
